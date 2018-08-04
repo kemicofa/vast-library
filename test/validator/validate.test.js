410496214eb6ac18b6c6eee8b96a744ccb97bc1e
David B <babel.david@gmail.com>
2018-08-04 12:37:40 +0200

    add error case on parse xml fails

--- test/validator/validate.test.js
+++ test/validator/validate.test.js
@@ -1,15 +1,36 @@
-const { assert } = require("chai");
+const { assert } = require('chai');
 
-const vastLib = require("../../lib/index");
-const { getFixtureContent } = require("../helpers");
+const vastLib = require('../../lib/index');
+const { getFixtureContent } = require('../helpers');
 
-describe("validate", () => {
-  it("should validate a correct VAST", () => {
-    const vastRaw = getFixtureContent("v4", "minimal_vast");
+describe('validate', () => {
+  it('should validate a correct VAST', () => {
+    const vastRaw = getFixtureContent('v4', 'minimal_vast');
     assert.isTrue(vastLib.validate(vastRaw));
   });
-  it("should reject an  incorrect VAST", () => {
-    const vastRaw = getFixtureContent("v4", "vast_missing_values");
+  it('should reject an incorrect VAST', () => {
+    const vastRaw = getFixtureContent('v4', 'vast_missing_values');
     assert.isFalse(vastLib.validate(vastRaw));
+  });
+  it('should reject an incorrect formated VAST', () => {
+    const vastRaw = '<valkjdlqksj>dmlsqkd<//dqsd>sd<<sd';
+    assert.isFalse(vastLib.validate(vastRaw));
+  });
+  it('should reject an incorrect formated VAST with warn', () => {
+    const vastRaw = '<valkjdlqksj>dmlsqkd<//dqsd>sd<<sd';
+
+    global.console = {
+      error: jest.fn()
+    };
+
+    const yellow = '\x1b[33m';
+    const red = '\x1b[31m';
+    const reset = '\x1b[0m';
+    const intro = `${yellow}VAST-BUILDER ${red}ERROR${yellow}:${reset}`;
+
+    assert.isFalse(vastLib.validate(vastRaw, { logWarn: true }));
+    expect(global.console.error).toHaveBeenCalledWith(
+      `${intro} Error during the vast parsing, it seems not valid XML`
+    );
   });
 });

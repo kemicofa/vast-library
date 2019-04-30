db676773b3fc6403fa7be768e664c9e0d8880420
David B <babel.david@gmail.com>
2019-04-30 11:08:32 +0200

    reenable request

--- common/utils/vast.ts
+++ common/utils/vast.ts
@@ -69,13 +69,13 @@ function fetchUrl({
     }
     throw new Error(`${url} was not found`);
   } else if (isNode) {
-    // const request = require("request");
-    // request(url, (error, response, body) => {
-    //   if (error) {
-    //     fail();
-    //   }
-    //   loadCallback(body);
-    // });
+    const request = require("request");
+    request(url, (error, response, body) => {
+      if (error) {
+        fail();
+      }
+      loadCallback(body);
+    });
   } else {
     throw new Error("Not supported environment");
   }

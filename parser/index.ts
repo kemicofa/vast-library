3aac4bb1fb258c22c46577acb86764f014d77c25
David B <babel.david@gmail.com>
2019-04-30 12:28:39 +0200

    refine api

--- parser/index.ts
+++ parser/index.ts
@@ -3,6 +3,7 @@ import { isNode } from "browser-or-node";
 import { isNull } from "../common/utils/checks";
 import { warnOrThrow } from "../common/utils/logs";
 import {
+  createVastWithBuilder,
   downloadVastAndWrappersAsync,
   downloadVastAndWrappersSync
 } from "../common/utils/vast";
@@ -11,43 +12,43 @@ import VastElement from "../common/vast-element";
 type VastElements = Array<VastElement<any>>;
 
 export default class VastParser {
-  public vasts: VastElements;
-  public parsed: boolean;
-  public originalVastUrl: string;
-  public options: VastParserOptions;
+  private options: VastParserOptions;
+  private vasts: VastElements;
 
-  constructor(vastUrl: string, options: VastParserOptions = {}) {
-    this.originalVastUrl = vastUrl;
+  constructor(options: VastParserOptions = {}) {
     this.options = options;
+    this.vasts = [];
   }
 
-  public parseAsync(callback: (self: this) => void) {
-    if (this.parsed) {
-      callback(this);
-    }
+  /**
+   * Usefull for tests, this method does not fetch anything. You can add wrappers and inlines manually.
+   */
+  public addVastWithoutFetching(vastXML: string) {
+    this.vasts.push(createVastWithBuilder(vastXML));
+  }
+
+  public cleanVasts() {
+    this.vasts = [];
+  }
+
+  public parseAsync(vastUrl: string, callback: (self: this) => void) {
+    this.cleanVasts();
     downloadVastAndWrappersAsync(
-      this.originalVastUrl,
+      vastUrl,
       this.options,
       (vasts: VastElements) => {
         this.vasts = vasts;
-        this.parsed = true;
         callback(this);
       }
     );
   }
 
-  public parseSync(): this {
+  public parseSync(vastUrl: string): this {
+    this.cleanVasts();
     if (isNode) {
       throw new Error("parseSync is only available in a browser context");
     }
-    if (this.parsed) {
-      return this;
-    }
-    this.vasts = downloadVastAndWrappersSync(
-      this.originalVastUrl,
-      this.options
-    );
-    this.parsed = true;
+    this.vasts = downloadVastAndWrappersSync(vastUrl, this.options);
     return this;
   }
 
@@ -58,60 +59,35 @@ export default class VastParser {
   public getVastElements(
     arrayOfTagNames: /* PossibleTag[] */ string[]
   ): VastElements {
-    return this.get(arrayOfTagNames);
+    const vastElements = (flatten(
+      this.vasts.map(v => v.get(arrayOfTagNames, true))
+    ) as unknown) as VastElements;
+
+    return vastElements;
   }
 
   public getContents(arrayOfTagNames: /* PossibleTag[] */ string[]): string[] {
-    return this.get(arrayOfTagNames, "content");
+    return this.getVastElements(arrayOfTagNames).map(vastElement => {
+      if (isNull(vastElement.content)) {
+        warnOrThrow(`${vastElement.name} does not have content`, this.options);
+      }
+      return vastElement.content;
+    });
   }
 
   public getAttributes(
     arrayOfTagNames: /* PossibleTag[] */ string[],
     attribute: string
   ): string[] {
-    return this.get(arrayOfTagNames, attribute);
-  }
-
-  // TODO turn into private
-  // > return an array all childs find at "arrayOfTagNames" path in the hierarchy
-  // * getChilds(arrayOfTagNames: Array<string>, details: 'content' | string): Array<VastElement>
-  public get(arrayOfTagNames: /* PossibleTag[] */ string[]): VastElements;
-  public get(
-    arrayOfTagNames: /* PossibleTag[] */ string[],
-    details: "content" | /* "PossibleAttrs" */ string
-  ): string[];
-  public get(
-    arrayOfTagNames: /* PossibleTag[] */ string[],
-    details?: "content" | /* "PossibleAttrs" */ string
-  ) {
-    const vastElements = (flatten(
-      this.vasts.map(v => v.get(arrayOfTagNames, true))
-    ) as unknown) as VastElements;
-    if (details) {
-      return vastElements.map(vastElement => {
-        if (details === "content") {
-          if (isNull(vastElement.content)) {
-            warnOrThrow(
-              `${vastElement.name} does not have content`,
-              this.options
-            );
-          }
-          return vastElement.content;
-        } else {
-          const attributeLowerCased = {};
-          Object.keys(vastElement.attrs).forEach(attr => {
-            attributeLowerCased[attr.toLowerCase()] = vastElement.attrs[attr];
-          });
-          if (isNull(attributeLowerCased[details.toLowerCase()])) {
-            warnOrThrow(
-              `${vastElement.name} does not have content`,
-              this.options
-            );
-          }
-          return attributeLowerCased[details.toLowerCase()];
-        }
+    return this.getVastElements(arrayOfTagNames).map(vastElement => {
+      const attributeLowerCased = {};
+      Object.keys(vastElement.attrs).forEach(attr => {
+        attributeLowerCased[attr.toLowerCase()] = vastElement.attrs[attr];
       });
-    }
-    return vastElements;
+      if (isNull(attributeLowerCased[attribute.toLowerCase()])) {
+        warnOrThrow(`${vastElement.name} does not have content`, this.options);
+      }
+      return attributeLowerCased[attribute.toLowerCase()];
+    });
   }
 }
--- tests/parser/parser.test.ts
+++ tests/parser/parser.test.ts
@@ -11,18 +11,20 @@ function getFileContent(fixturePath: string) {
 
 function getVasts() {
   return [
-    createVastWithBuilder(getFileContent("minimal_wrapper_3.xml")),
-    createVastWithBuilder(getFileContent("minimal_wrapper_2.xml")),
-    createVastWithBuilder(getFileContent("minimal_wrapper_1.xml")),
-    createVastWithBuilder(getFileContent("minimal_vast.xml"))
+    getFileContent("minimal_wrapper_3.xml"),
+    getFileContent("minimal_wrapper_2.xml"),
+    getFileContent("minimal_wrapper_1.xml"),
+    getFileContent("minimal_vast.xml")
   ];
 }
 
 describe("VastParser", () => {
   let parser: VastParser;
   beforeEach(() => {
-    parser = new VastParser("");
-    parser.vasts = getVasts();
+    parser = new VastParser();
+    getVasts().forEach(vastXML => {
+      parser.addVastWithoutFetching(vastXML);
+    });
   });
   test("should ", () => {
     // TODO do tests :)
